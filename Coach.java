7e643e08091ffc0cec2bfce0108120afd2913825
cdp04 <74835103+cdp04@users.noreply.github.com>
2020-11-21 11:16:35 -0800

    Update Coach.java

--- Coach.java
+++ Coach.java
@@ -1,8 +1,11 @@
 package teamProject;
 
 //@author Nathaniel Taylor
+//@author Carlos De León Polanco.
 //@author Benny Rodriguez
+
 //This class represents a basketball coach
+
 public class Coach {
 	private String name;
 	private int age;
