673606ba88357890c5339fe6ca9f6bddf78d6e3f
cdp04 <74835103+cdp04@users.noreply.github.com>
2020-11-21 11:15:08 -0800

    Update Athletes.java

--- Athletes.java
+++ Athletes.java
@@ -1,12 +1,14 @@
 package teamProject;
 
+//@ author Carlos De León Polanco.
 //@author Nathaniel Taylor
 //@author Benny Rodriguez
+
 public abstract class Athletes {
 	private String name;
 	private int age;
 	private String id;
-	
+
 	public Athletes(String name, int age, String id) {
 		this.name = name;
 		this.age = age;
