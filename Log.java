228bfef2d7500d26d3b31d276b0c9c06f529e3d6
Akmanos <nathanieltaylor515@gmail.com>
2020-11-21 14:10:44 -0500

    Update Log.java

--- Log.java
+++ Log.java
@@ -1,5 +1,4 @@
 package teamProject;
-import java.io.File;
 import java.io.*;
 
 //@author Nathaniel Taylor
@@ -7,5 +6,21 @@ import java.io.*;
 public class Log {
 	private String id;
 	private String password;
+	private File file;
 	
+	Log(String id, String password) throws IOException{
+		this.id = id;
+		this.password = password;
+		//create a new file
+		try {
+			file = new File("log.txt");
+			if(file.createNewFile()) 
+				System.out.println("File " + file.getName() + " created");
+			else
+				System.out.println("File " + file.getName() + " already exists");
+			
+		}catch(IOException e) {
+			throw new IOException("Error creating file");
+		}
+	}
 }
