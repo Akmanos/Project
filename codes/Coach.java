c6902a5436b2eadd0034b8526337dcdc66fa52ef
cdp04 <74835103+cdp04@users.noreply.github.com>
2020-12-10 22:40:23 -0800

    Update Coach.java

--- codes/Coach.java
+++ codes/Coach.java
@@ -1,11 +1,45 @@
 package teamProject;
 
+
 //@author Nathaniel Taylor
 //@author Benny Rodriguez
 //@author Carlos De León Polanco.
 //This class represents a basketball coach
-public class Coach extend Athletes{
-	private String name;
-	private int age;
-	
+
+public class Coach extends Athletes{
+    private String type;
+
+    public Coach(String name, int age, String id){
+        super(name, age, id);
+        type = "Coach";
+    }
+
+    protected void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    protected void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    protected void setID(String id){
+        this.id = id;
+    }
+
+    public String getID(){
+        return id;
+    }
+
+    public String getType(){
+        return type;
+    }
+
 }
