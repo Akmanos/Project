34281cd47507e0841ac5b021a267e2be81fdff67
SirBenneh <74834528+SirBenneh@users.noreply.github.com>
2020-12-16 18:29:48 -0500

    Add files via upload

--- codes/Schedule.java
+++ codes/Schedule.java
@@ -5,5 +5,28 @@
 //@author Carlos De León Polanco.
 //this class represents a season's schedule
 public class Schedule {
+    
+    private String type = "Default";
+    private String date = "12/31";
+    
+    public Schedule(String dayType, String date){
+        this.type = dayType;
+        this.date = date;
+    }
 
+    public String getDayType(){
+        return this.type;
+    }
+
+    public void setDayType(String dayt){
+        this.type = dayt;
+    }
+
+    public String getDate(){
+        return this.date;
+    }
+
+    public void setDate(String datet){
+        this.date = datet;
+    }
 }
